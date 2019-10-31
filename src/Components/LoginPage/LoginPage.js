9592e2459c4ee435515d999c98712a79450d00fc
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-10-31 08:35:34 +0100

    Initial commit

---
+++ src/Components/LoginPage/LoginPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import "./LoginPage.scss"
+
+class LoginPage extends Component {
+    state = {}
+    render() {
+        return (
+            <div class="login-page-wrapper">
+                <input class="login-input"></input>
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
